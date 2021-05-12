80ac2bfd0c6d274a9587318a8aa163bffc6e07b3
hushishuai <hushishuai.he@chinatelecom.cn>
2021-05-12 15:24:06 +0800

    下拉选择框

--- selenium/selectBox.py
+++ selenium/selectBox.py
@@ -7,6 +7,10 @@
 ------------      -------    --------    -----------
 2020/12/30 14:54   gujiayue      1.0         处理网页中下拉框的
 '''
+#方法1：直接定位到下拉框进行选择。一般适用于下拉框的标签名称就是select，selenium给了有力的支持，就是Select类
+#详细参考：https://www.cnblogs.com/z-x-y/p/9020833.html。
+
+
 import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
@@ -21,5 +25,8 @@ s1.select_by_index(1)  # 选择第二项选项：o1
 time.sleep(10)
 s1.select_by_value("o2")  # 选择value="o2"的项
 s1.select_by_visible_text("o3")  # 选择text="o3"的值，即在下拉时我们可以看到的文本
-
 driver.quit()
+
+#方法2：针对下拉框的标签名称是input
+#针对该种类型的处理，首先定位到该下拉input元素，然后鼠标点击，这时候就把下拉框选项都展示出来了
+#然后，定位到要选择的选项元素，再次click，就完成了选择
